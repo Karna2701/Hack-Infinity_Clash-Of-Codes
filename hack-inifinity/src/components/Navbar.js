50c4833e08613aa99bfa3f3aaa55e8f51caffcde
janvimehta25 <janvimehta103@gmail.com>
2023-03-04 18:25:35 +0530

    first commit

--- hack-inifinity/src/components/Navbar.js
+++ hack-inifinity/src/components/Navbar.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 
 const Navbar = () => {
     let Links = [
-        { name: "BENEFITS", link: "/" },
+        { name: "Benefits", link: "/" },
         { name: "How it Works", link: "/" },
 
        
@@ -12,8 +12,7 @@ const Navbar = () => {
     return (
         <div className='shadow-md w-full fixed top-0 left-0 bg-black'>
             <div className='md:flex items-center justify-between bg-white py-4 md:px-10 px-7'>
-                <div className='font-bold text-2xl cursor-pointer flex items-center font-[Poppins] 
-      text-gray-800'>
+                <div className='font-bold text-2xl cursor-pointer flex items-center font-[Poppins] text-gray-800'>
                     <span className='text-3xl text-indigo-600 mr-1 pt-2'>
                         <ion-icon name="logo-ionic"></ion-icon>
                     </span>
@@ -24,22 +23,25 @@ const Navbar = () => {
                     <ion-icon name={open ? 'close' : 'menu'}></ion-icon>
                 </div>
 
-                <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ' : 'top-[-490px]'}`}>
-                    {
-                        Links.map((link) => (
-                            <li key={link.name} className='md:ml-8 text-xl md:my-0 my-7'>
-                                <a href={link.link} className='text-gray-800 hover:text-gray-400 duration-500'>{link.name}</a>
-                            </li>
-                        ))
-                    }
+                <div class="hidden md:flex items-center space-x-1">
+                    <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ' : 'top-[-490px]'}`}>
+                        {
+                            Links.map((link) => (
+                                <li key={link.name} className='md:ml-8 text-xl md:my-0 my-7'>
+                                    <a href={link.link} className='text-gray-800 hover:text-gray-400 duration-500'>{link.name}</a>
+                                </li>
+                            ))
+                        }
 
-                </ul>
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
-  SIGN UP
-</button>
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
-  LOGIN
-</button>
+                    </ul>
+                </div>
+                
+                <div className="hidden space-x-6 md:inline-block">
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 border border-blue-700 rounded">
+                    LOGIN</button>
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 border border-blue-700 rounded">
+                    SIGN UP</button>
+                </div>
             </div>
         </div>
     )
