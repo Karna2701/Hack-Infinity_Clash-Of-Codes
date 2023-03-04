6eb635bb3d274453ac3d5c68a04b0cc2fd0f1a5e
prem <premdoshi1234@gmail.com>
2023-03-04 19:22:50 +0530

    message text

--- hack-inifinity/src/components/DragDropFiles.js
+++ hack-inifinity/src/components/DragDropFiles.js
@@ -1,13 +1,19 @@
-import React from 'react';
+
+import React,{useState,useEffect} from 'react'
 
 
 const DragDropFiles = () => {
+  const [images, setImages] = useState([]);
+  console.log("ndnd");
+  function onImageChange(e) {
+    setImages([...e.target.files]);
+  }
+  return(
   <>
-    
-<input type="file" id="myFile" name="filename"></input>
-
-
-  </>
+    <input type="file" multiple accept="image/*" onChange={onImageChange}/>
+ 
+    </>
+  )
 }
 
 export default DragDropFiles
